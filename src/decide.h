7f275eb4b0081946e25615b5c2c6b9fa41dc2369
Hannes Stig <hannes.a.stig@gmail.com>
2024-01-27 01:45:32 +0100

    Created a headerfile that uses a class for decide instead of a global namespace. #22 #1

--- src/decide.h
+++ src/decide.h
@@ -1,91 +1,80 @@
 #ifndef DECIDE_H
 #define DECIDE_H
 
-#include <cmath>
+#include <array>
+#include <vector>
 
-namespace Decide {
-enum CONNECTORS {
-    NOTUSED, ANDD, ORR
+enum CONNECTORS { NOTUSED = 777, ORR, ANDD };
+
+// Less than, equal to, greater than
+enum COMPTYPE { LT = 1111, EQ, GT };
+
+struct COORDINATE {
+  double x;
+  double y;
 };
-
-typedef double *COORDINATE;
-
-typedef CONNECTORS **CMATRIX;
-
-typedef bool **BMATRIX;
-
-typedef bool *VECTOR;
-
-enum COMPTYPE {LT, EQ, GT};
 
 struct PARAMETERS_T {
-    double LENGTH1; // Length in LICs 0, 7, 12
-    double RADIUS1; // Radius in LICs 1, 8, 13
-    double EPSILON; // Deviation from PI in LICs 2, 9
-    double AREA1; // Area in LICs 3, 10, 14
-    int Q_PTS; // No. of consecutive points in LIC 4
-    int QUADS; // No. of quadrants in LIC 4
-    double DIST; // Distance in LIC 6
-    int N_PTS; // No. of consecutive pts. in LIC 6
-    int K_PTS; // No. of int. pts. in LICs 7, 12
-    int A_PTS; // No. of int. pts. in LICs 8, 13
-    int B_PTS; // No. of int. pts. in LICs 8, 13
-    int C_PTS; // No. of int. pts. in LICs 9
-    int D_PTS; // No. of int. pts. in LICs 9
-    int E_PTS; // No. of int. pts. in LICs 10, 14
-    int F_PTS; // No. of int. pts. in LICs 10, 14
-    int G_PTS; // No. of int. pts. in LICs 11
-    double LENGHT2; // Maximum length in LIC 12
-    double RADIUS2; // Maximum radius in LIC 13
-    double AREA2; // Maximum area in LIC 14
+  double LENGTH1; // Length in LICs 0, 7, 12
+  double RADIUS1; // Radius in LICs 1, 8, 13
+  double EPSILON; // Deviation from PI in LICs 2, 9
+  double AREA1;   // Area in LICs 1, 8, 13
+  int Q_PTS;      // No. of consecutive points in LIC 4
+  int QUADS;      // No. of quadrants in LIC 4
+  double DIST;    // Distance in LIC 6
+  int N_PTS;      // No. of consecutive pts. in LIC 6
+  int K_PTS;      // No. of int. pts. in LICs 7, 12
+  int A_PTS;      // No. of int. pts. in LICs 8, 13
+  int B_PTS;      // No. of int. pts. in LICs 8, 13
+  int C_PTS;      // No. of int. pts. in LIC 9
+  int D_PTS;      // No. of int. pts. in LIC 9
+  int E_PTS;      // No. of int. pts. in LICS 10, 14
+  int F_PTS;      // No. of int. pts. in LICS 10, 14
+  int G_PTS;      // No. of int. pts. in LIC 11
+  double LENGTH2; // Maximum length in LIC 12
+  double RADIUS2; // Maximum radius in LIC 13
+  double AREA2;   // Maximum area in LIC 14
 };
 
+class Decide {
+private:
+  // Inputs
+  const int NUMPOINTS; // Number of planar data points.
+  const std::vector<COORDINATE> COORDINATES; // Array containing the coordinates of data points.
+  const PARAMETERS_T PARAMETERS; // Struct holding the parameters for LICs.
+  const std::array<std::array<CONNECTORS, 15>, 15> LCM;  // Logical Connector Matrix. IMPORTANT! LCM[y][x] <-- y first then x.
+  const std::array<bool, 15> PUV; // Preliminary unlocking vector.
 
-// Global variable declarations
+  // Outputs
+  bool LAUNCH;
+  std::array<bool, 15> CMV; // Conditions Met Vector.
+  std::array<std::array<bool, 15>, 15> PUM; // Preliminary Unlocking Matrix. IMPORTANT! PUM[y][x] <-- y first then x.
+  std::array<bool, 15> FUV; // Final Unlocking Vector.
 
-PARAMETERS_T PARAMETERS;
-// static PARAMETERS_T PARAMETERS_2;
+  // Methods
+  // Method for comparing doubles.
+  COMPTYPE DOUBLECOMPARE(double A, double B) const;
+  
+  // Step 2.1 from specification.
+  void Calc_CMV();
+  
+  // Step 2.2 from specification.
+  void Calc_PUM();
+  
+  // Step 2.3 from specification.
+  void Calc_FUV();
+  
+  // 2.4 from specification.
+  void Calc_LAUNCH();
 
-// X coordinates of data points
-COORDINATE X;
-// static COORDINATE X2;
+public:
+  Decide(int NUMPOINTS, const std::vector<COORDINATE> &POINTS,
+         const PARAMETERS_T &PARAMETERS,
+         const std::array<std::array<CONNECTORS, 15>, 15> &LCM,
+         const std::array<bool, 15> &PUV);
 
-// Y coordinates of data points
-COORDINATE Y;
-// static COORDINATE Y2;
+  // Call functions for 2.1 - 2.4 and print answer to stdout.
+  void decide();
+};
 
-// Number of data points
-int NUMPOINTS;
-// static int NUMPOINTS2;
-
-// Logical connector matrix
-CMATRIX LCM;
-// static CMATRIX LCM;
-
-// Preliminary unlocking matrix;
-BMATRIX PUM;
-// static BMATRIX PUM2;
-
-// Conditions met vector
-VECTOR CMV;
-// static VECTOR CMV2;
-
-
-// Final unlocking vector
-VECTOR FUV;
-// static VECTOR FUV2;
-
-// Decision : Launch or No Launch
-bool LAUNCH;
-// static bool LAUNCH2;
-
-// compares floating point numbers
-static inline COMPTYPE DOUBLECOMPARE(double A, double B) {
-    if(fabs(A - B) < 0.000001) return EQ;
-    if(A < B) return LT;
-    return GT;
-}
-void DECIDE(void);
-}
-
-#endif // DECIDE
+#endif
