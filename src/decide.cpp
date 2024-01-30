a9efe5c5bb52cc0173abcc5556b5c43d5b5775d2
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-01-30 19:14:05 +0100

    LIC for #6 #13 #18 (#25)
    * add CMV1 calc #6
    
    * add CMV6 calc #13
    
    * add CMV6 calc #18
    
    * refactor void to bool for LIC calculation #6 #13 #18
    
    * fix typos in refactor, remove directly editing CMV #18

--- src/decide.cpp
+++ src/decide.cpp
@@ -3,10 +3,8 @@
 #include <cstdio>
 
 COMPTYPE Decide::DOUBLECOMPARE(double a, double b) const {
-  if (fabs(a - b) < 0.000001)
-    return EQ;
-  if (a < b)
-    return LT;
+  if (fabs(a - b) < 0.000001) return EQ;
+  if (a < b) return LT;
   return GT;
 }
 
@@ -14,8 +12,11 @@ Decide::Decide(int NUMPOINTS, const std::vector<COORDINATE> &POINTS,
                const PARAMETERS_T &PARAMETERS,
                const std::array<std::array<CONNECTORS, 15>, 15> &LCM,
                const std::array<bool, 15> &PUV)
-    : NUMPOINTS(NUMPOINTS), COORDINATES(POINTS), PARAMETERS(PARAMETERS),
-      LCM(LCM), PUV(PUV) {}
+    : NUMPOINTS(NUMPOINTS),
+      COORDINATES(POINTS),
+      PARAMETERS(PARAMETERS),
+      LCM(LCM),
+      PUV(PUV) {}
 
 void Decide::debugprint() const {
   printf("Coordinates (x, y):\n");
@@ -86,7 +87,34 @@ void Decide::debugprint() const {
 
 void Decide::Lic0() {}
 
-void Decide::Lic1() {}
+/**
+ * @brief There exists at least one set of three consecutive data points that
+ * cannot all be contained within or on a circle of radius RADIUS1.
+ *
+ * https://artofproblemsolving.com/wiki/index.php/Circumradius
+ */
+bool Decide::Lic1() {
+  for (int i = 0; i < NUMPOINTS - 2; ++i) {
+    COORDINATE p1 = COORDINATES[i];
+    COORDINATE p2 = COORDINATES[i + 1];
+    COORDINATE p3 = COORDINATES[i + 2];
+
+    // find the size of the triangle
+    double a = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+    double b = sqrt(pow(p2.x - p3.x, 2) + pow(p2.y - p3.y, 2));
+    double c = sqrt(pow(p3.x - p1.x, 2) + pow(p3.y - p1.y, 2));
+
+    double s = (a + b + c) / 2;
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));  // Heron's formula
+
+    double r = (a * b * c) / (4 * area);  // radius of the circumcircle
+
+    if (DOUBLECOMPARE(r, PARAMETERS.RADIUS1) == GT) {
+      return true;
+    }
+  }
+  return false;
+}
 
 void Decide::Lic2() {}
 
@@ -96,7 +124,56 @@ void Decide::Lic4() {}
 
 void Decide::Lic5() {}
 
-void Decide::Lic6() {}
+/**
+ * @brief There exists at least one set of N_PTS consecutive data points such
+ * that at least one of the points lies a distance greater than DIST from the
+ * line joining the first and last of these N_PTS points. If the first and last
+ * points of these N_PTS are identical, then the calculated distance to compare
+ * with DIST will be the distance from the coincident point to all other points
+ * of the N_PTS consecutive points. The condition is not met when NUMPOINTS < 3.
+ *
+ */
+bool Decide::Lic6() {
+  if (NUMPOINTS < 3) {
+    return false;
+  }
+
+  for (int i = 0; i < NUMPOINTS - PARAMETERS.N_PTS + 1; ++i) {
+    COORDINATE p1 = COORDINATES[i];
+    COORDINATE p2 = COORDINATES[i + PARAMETERS.N_PTS - 1];
+
+    if (DOUBLECOMPARE(p1.x, p2.x) == EQ && DOUBLECOMPARE(p1.y, p2.y) == EQ) {
+      // p1 and p2 are the same point
+      for (int j = 0; j < NUMPOINTS; ++j) {
+        if (j == i) continue;
+
+        COORDINATE p3 = COORDINATES[j];
+
+        double distance = sqrt(pow(p3.x - p1.x, 2) + pow(p3.y - p1.y, 2));
+
+        if (DOUBLECOMPARE(distance, PARAMETERS.DIST) == GT) {
+          return true;
+        }
+      }
+    } else {
+      // p1 and p2 are different points
+      for (int j = 0; j < NUMPOINTS; ++j) {
+        if (j == i || j == i + PARAMETERS.N_PTS - 1) continue;
+
+        COORDINATE p3 = COORDINATES[j];
+        // https://math.stackexchange.com/questions/2757318/distance-between-a-point-and-a-line-defined-by-2-points
+        double distance = fabs((p2.x - p1.x) * (p3.y - p1.y) -
+                               (p3.x - p1.x) * (p2.y - p1.y)) /
+                          sqrt(pow(p2.y - p1.y, 2) + pow(p2.x - p1.x, 2));
+
+        if (DOUBLECOMPARE(distance, PARAMETERS.DIST) == GT) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
 
 void Decide::Lic7() {}
 
@@ -106,7 +183,29 @@ void Decide::Lic9() {}
 
 void Decide::Lic10() {}
 
-void Decide::Lic11() {}
+/**
+ * @brief There exists at least one set of two data points, (X[i],Y[i]) and
+ * (X[j],Y[j]), separated by exactly G_PTS consecutive intervening points, such
+ * that X[j] - X[i] < 0. (where i < j ) The condition is not met when NUMPOINTS
+ * < 3. 1 ≤ G PTS ≤ NUMPOINTS−2
+ *
+ */
+bool Decide::Lic11() {
+  if (NUMPOINTS < 3) {
+    return false;
+  }
+
+  for (int i = 0; i < NUMPOINTS - PARAMETERS.G_PTS - 1; ++i) {
+    COORDINATE p1 = COORDINATES[i];
+    COORDINATE p2 = COORDINATES[i + PARAMETERS.G_PTS + 1];
+
+    if (DOUBLECOMPARE(p2.x - p1.x, 0) == LT) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 void Decide::Lic12() {}
 
