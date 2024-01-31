460587f19454c6ed1b839e3c640940106e13f149
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-01-31 11:54:57 +0100

    add test cases for LIC 11 #42

--- test/CMVTest.cpp
+++ test/CMVTest.cpp
@@ -70,4 +70,35 @@ TEST(CMV, LIC6) {
 
   EXPECT_EQ(decideB.Lic6(), false);
 }
-TEST(CMV, LIC11) {}
+TEST(CMV, LIC11) {
+  std::vector<COORDINATE> pointsP = {{5, 0}, {3, 0}, {4, 4}};
+  PARAMETERS_T paramP;
+  paramP.G_PTS = 1;
+  std::array<std::array<CONNECTORS, 15>, 15> lcmP = {NOTUSED};
+  std::array<bool, 15> puvP = {0};
+
+  Decide decideP(pointsP.size(), pointsP, paramP, lcmP, puvP);
+
+  EXPECT_EQ(decideP.Lic11(), true);
+
+  std::vector<COORDINATE> pointsN = {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}};
+  PARAMETERS_T paramN;
+  paramN.G_PTS = 3;
+  std::array<std::array<CONNECTORS, 15>, 15> lcmN;
+  std::array<bool, 15> puvN;
+
+  Decide decideN(pointsN.size(), pointsN, paramN, lcmN, puvN);
+
+  EXPECT_EQ(decideN.Lic11(), false);
+
+  std::vector<COORDINATE> pointsB = {{0, 0}, {3, 0}, {0, 4}};
+  PARAMETERS_T paramB;
+  paramB.G_PTS = 1;
+
+  std::array<std::array<CONNECTORS, 15>, 15> lcmB;
+  std::array<bool, 15> puvB;
+
+  Decide decideB(pointsB.size(), pointsB, paramB, lcmB, puvB);
+
+  EXPECT_EQ(decideB.Lic11(), false);
+}
